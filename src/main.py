fbe0d613e6de2f549cd4d74a12981f7132d1538d
Adrian Dąbek <adrian950113@gmail.com>
2021-01-21 14:44:57 +0100

    prrof of concept

--- src/main.py
+++ src/main.py
@@ -4,28 +4,6 @@ from typing import List, Optional, Union, KeysView
 import random
 import uuid
 
-INPUT_DATA = """
-alter table
-create table client_details
-(
-	type varchar(31) not null,
-	id uuid not null
-		constraint client_details_pkey
-			primary key,
-	cif varchar(255),
-	krs varchar(255),
-	name varchar(255),
-	nip varchar(255),
-	pesel varchar(255),
-	regon varchar(255),
-	surname varchar(255),
-	attorney varchar(255)
-);
-add index
-"""
-
-single_table_definition = INPUT_DATA
-
 
 class DataGenerator:
     def generate(self):
@@ -79,53 +57,66 @@ class Relation:
         pass
 
 
-def process_input_file(ddl: str):
-    ddl_lines = ddl.splitlines()
-    relations = []
-    table_name_regex = re.compile("create table ([a-z0-9_]+)", re.IGNORECASE)
-    columns = []
+class FileParser:
+    def parse(self, filepath: str) -> List[Relation]:
+        relations = []
+        definition_start_regex = re.compile("create table ([a-z0-9_]+)", re.IGNORECASE)
+        definition_end_regex = re.compile("\);")
+        stage = 0
+        STAGE_SEARCHING = 0
+        STAGE_READING = 1
+        columns = []
 
-    for line in ddl_lines:
-        match = table_name_regex.match(line)
-        if match is not None:
-            relations.append(Relation(match.group(0)))
-        if len(relations) > 0:
-            column = parse_column_dd(line.split())
-            if column is not None:
-                columns.append(column)
-    relations[-1].columns = columns
-    return relations
+        with open(filepath, "r") as file:
+            line = file.readline()
+            while line:
+                if stage == STAGE_SEARCHING:
+                    match = definition_start_regex.match(line)
+                    if match is not None:
+                        r = Relation(match.group(0))
+                        relations.append(r)
+                        stage = STAGE_READING
+                elif stage == STAGE_READING:
+                    if definition_end_regex.match(line) is not None:
+                        relations[-1].columns = columns
+                        stage = STAGE_SEARCHING
+                    elif len(relations) > 0:
+                        column = self.__parse_column_ddl(line.split())
+                        if column is not None:
+                            columns.append(column)
+                line = file.readline()
+        return relations
+
+    def __parse_column_ddl(self, ddl: List[str]) -> Optional[Column]:
+        if len(ddl) < 2:
+            return None
+
+        name_test = re.compile("[a-z0-9_]+", re.IGNORECASE)
+        if name_test.match(ddl[0]) is None:
+            return None
+
+        type_test = re.compile("([a-z]+)(\(([0-9]+)\))?", re.IGNORECASE)
+        type_match = type_test.match(ddl[1])
+
+        if type_match is None:
+            return None
+
+        name = ddl[0]
+        type = type_match.group(1).lower()
+        type_argument = type_match.group(3)
+
+        factory = GeneratorFactory()
+        SUPPORTED_TYPES = factory.supported_types()
+
+        if type not in SUPPORTED_TYPES:
+            return None
+
+        data_generator = factory.create(type, type_argument)
+        return Column(name, data_generator, False)
 
 
-def parse_column_dd(ddl: List[str]) -> Optional[Column]:
-    if len(ddl) < 2:
-        return None
-
-    name_test = re.compile("[a-z0-9_]+", re.IGNORECASE)
-    if name_test.match(ddl[0]) is None:
-        return None
-
-    type_test = re.compile("([a-z]+)(\(([0-9]+)\))?", re.IGNORECASE)
-    type_match = type_test.match(ddl[1])
-
-    if type_match is None:
-        return None
-
-    name = ddl[0]
-    type = type_match.group(1).lower()
-    type_argument = type_match.group(3)
-
-    factory = GeneratorFactory()
-    SUPPORTED_TYPES = factory.supported_types()
-
-    if type not in SUPPORTED_TYPES:
-        return None
-
-    data_generator = factory.create(type, type_argument)
-    return Column(name, data_generator, False)
-
-
-relations = process_input_file(INPUT_DATA)
+parser = FileParser()
+relations = parser.parse("data1.sql")
 for r in relations:
     for c in r.columns:
         print(c.name, c.generator.generate())
